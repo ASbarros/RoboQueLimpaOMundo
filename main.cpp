25c64ac471a650aaf38a404800601a85b80d3e5b
asbarros <anderson.barros0298@gmail.com>
2019-11-28 12:48:42 -0300

    cruzamento e mutacao

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include<time.h>/// necessário p/ função time() ...
 
-#define INDIVIDUOS 10
-#define PASSOS 20
+#define INDIVIDUOS 100
+#define PASSOS 200
 #define TOTAL_ITERACOES 100
-#define TOTAL_MATRIZES 2
+#define TOTAL_MATRIZES TOTAL_ITERACOES
 
 using namespace std;
 
@@ -14,16 +14,14 @@ const int linha = 10;
 const int coluna = 10;
 int matMundo[12][12];
 
-int numAleatorio2()
-{
-    return rand() % 2;
-}
+int posicoes[INDIVIDUOS];
 
-int numAleatorio7()
-{
-    return rand() % 7;
-}
 
+
+int numAleatorio(int range)
+{
+    return rand() % range;
+}
 
 void preencheMundo()
 {
@@ -44,7 +42,7 @@ void preencheMundo()
     {
         for(int j = 1; j <= coluna ; j++)
         {
-            matMundo[i][j] = numAleatorio2();
+            matMundo[i][j] = numAleatorio(2);
         }
     }
 }
@@ -62,8 +60,9 @@ void imprimeMatrizMundo()
 }
 
 
-void imprimeMatrizIndividuos(int **mat)
+void imprimeMatrizIndividuos(int **mat, int geracao)
 {
+    printf("\n---------  geracao %d  ---------------\n", geracao+1);
     for(int i = 0; i < INDIVIDUOS; i++)
     {
         for(int j = 0; j < PASSOS; j++)
@@ -72,6 +71,7 @@ void imprimeMatrizIndividuos(int **mat)
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int pontuacao(int linha)
@@ -90,19 +90,57 @@ void geraPontuacao(int **mat)
     {
         for(int j = 0; j < PASSOS; j++)
         {
-            mat[i][j] = numAleatorio7();
+            mat[i][j] = numAleatorio(7);
         }
     }
 }
 
-int pegaPontuacao(int **mat, int linha)
+int pegaPontuacao(int **mat, int l)
 {
     int soma = 0;
     for(int j = 0; j < PASSOS; j++)
     {
-        soma += mat[linha][j];
+        soma += mat[l][j];
     }
     return soma;
+}
+
+void proxGeracao(int **mat, int matProx[INDIVIDUOS][PASSOS], int l, int atual)
+{
+    for(int j = 0; j < PASSOS; j++)
+    {
+        matProx[atual][j] = mat[l][j];
+    }
+}
+
+void cruzamento (int **mat, int individuo1, int individuo2, int individuo3)
+{
+    for(int i = 0; i < PASSOS; i++)
+    {
+        if(i <= PASSOS/2)
+        {
+            mat[individuo3][i] = mat[individuo1][i];
+        }
+        else
+        {
+            mat[individuo3][i] = mat[individuo2][i];
+        }
+    }
+}
+
+void mutacao(int **mat)
+{
+    int aleatorio = 1;
+    for (int i = 0; i < INDIVIDUOS; i++)
+    {
+        for (int j = 0; j < PASSOS; j++)
+        {
+            aleatorio = numAleatorio(7);
+            if(aleatorio == 0)
+                aleatorio = 1;
+            mat[i][j] = (mat[i][j] * aleatorio) % 7;
+        }
+    }
 }
 
 int main()
@@ -110,12 +148,25 @@ int main()
     srand(time(NULL)); /// semente para gerar números aleátorios ...
 
     int **matIndividuos[TOTAL_MATRIZES];
+    int matAux[INDIVIDUOS][PASSOS];
+    int individuo1 = 0, individuo2 = 0, individuo3 = 0;
+    int ponIndividuo1 = 0, ponIndividuo2 = 0;
+
+    for (int i = 0; i < INDIVIDUOS; i++)
+    {
+        posicoes[i] = i;
+    }
+
     for(int i = 0; i < TOTAL_MATRIZES; i++)
     {
         matIndividuos[i] = new int*[INDIVIDUOS];
         for(int j = 0; j < INDIVIDUOS; j++)
         {
             matIndividuos[i][j] = new int[PASSOS];
+            for(int k=0; k < PASSOS; k++)
+            {
+                matIndividuos[i][j][k] = 0;
+            }
         }
     }
 
@@ -123,13 +174,47 @@ int main()
     geraPontuacao(matIndividuos[0]); /// preenchendo a matriz de pontuação ...
 
     imprimeMatrizMundo();
-    imprimeMatrizIndividuos(matIndividuos[0]);
-    for(int iteracao = 0; iteracao < TOTAL_ITERACOES; iteracao++)
+    imprimeMatrizIndividuos(matIndividuos[0], 0);
+    for(int iteracao = 1; iteracao < TOTAL_ITERACOES; iteracao++) /// for responsável por cada geração ...
     {
-        /// cruzamento ...
-        /// mutacao ...
-    }
+        for (int i = 0; i < INDIVIDUOS; i++)
+        {
+            for (int j = 0; j < PASSOS; j++)
+            {
+                matAux[i][j] = matIndividuos[iteracao-1][i][j];
+            }
+        }
 
-    //cout << pegaPontuacao(5);
+        for (int j = 0; j < INDIVIDUOS/2; j++) /// for para pegar preencher a nova matriz de individuos com os representantes da anterior ...
+        {
+
+            individuo1 = numAleatorio(INDIVIDUOS);
+            individuo2 = numAleatorio(INDIVIDUOS);
+
+            ponIndividuo1 = pegaPontuacao(matIndividuos[iteracao-1], individuo1);
+            ponIndividuo2 = pegaPontuacao(matIndividuos[iteracao-1], individuo2);
+            /// pegando os representantes ...
+
+            if(ponIndividuo1 > ponIndividuo2)
+                proxGeracao(matIndividuos[iteracao], matAux, individuo1, j);
+            else
+                proxGeracao(matIndividuos[iteracao], matAux, individuo2, j);
+        }
+        for (int j = INDIVIDUOS/2; j < INDIVIDUOS; j++) /// for para fazer o cruzamento ...
+        {
+            individuo1 = numAleatorio(INDIVIDUOS);
+            individuo2 = numAleatorio(INDIVIDUOS);
+            individuo3 = numAleatorio(INDIVIDUOS);
+
+            /// cruzamento ...
+            cruzamento(matIndividuos[iteracao], individuo1, individuo2, individuo3);
+        }
+
+        /// mutacao ...
+        mutacao(matIndividuos[iteracao]);
+
+        geraPontuacao(matIndividuos[iteracao]);
+        imprimeMatrizIndividuos(matIndividuos[iteracao], iteracao);
+    }
     return 0;
 }
